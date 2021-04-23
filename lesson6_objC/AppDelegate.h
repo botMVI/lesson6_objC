9714701454d95822f0235265cd0e6b8608896769
Mike <ilmivy1024@gmail.com>
2021-04-23 14:38:29 +0600

    Initial Commit

---
+++ lesson6_objC/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  lesson6_objC
+//
+//  Created by Michael Iliouchkin on 23.04.2021.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ lesson6_objC/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  lesson6_objC
+//
+//  Created by Michael Iliouchkin on 23.04.2021.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
